d76c35d4d3a9f5533f70195f7b95fbd232364529
jubayer1418 <skjubayer1418@gmail.com>
2024-03-25 19:48:21 +0600

    dd

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use("/", router);
 app.use(
   cors({
-    origin: ["https://smartphone-inventory-fronted-6.vercel.app/"],
+    origin: ["http://localhost:5173/"],
     credentials: true,
   })
 );
