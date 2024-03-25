b3056caed554cec534f3b5835afcf9b3fd175223
jubayer1418 <skjubayer1418@gmail.com>
2024-03-25 19:45:37 +0600

    dd

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,17 @@ import { notFound } from "./app/middleware/notFound";
 import { router } from "./app/routes";
 
 const app: Application = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/", router);
 app.use(
   cors({
     origin: ["https://smartphone-inventory-fronted-6.vercel.app/"],
     credentials: true,
   })
 );
-app.use(express.json());
-app.use(cookieParser());
-
-app.use("/", router);
-
 app.get("/", (req, res) => {
   res.send("Assignment-6");
 });
